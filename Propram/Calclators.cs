524fc4a2efb0774c6e1db4568262f2f961cc9231
andy110414 <aeeoprsd110414@gmail.com>
2019-10-31 18:55:34 +0800

    加入專案檔案。

---
+++ Propram/Calclators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Propram
+{
+    public class Calclators
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public float Div(float a, float b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Propram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Propram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calclators calc = new Calclators();
+            Console.WriteLine($"3+5={calc.Add(3, 5)}");
+            Console.WriteLine($"3-5={calc.Sub(3, 5)}");
+            Console.WriteLine($"3*5={calc.Mul(3, 5)}");
+            Console.WriteLine($"3/5={calc.Div(3, 5)}");
+
+            Console.ReadKey();
+        }
+    }
+}
