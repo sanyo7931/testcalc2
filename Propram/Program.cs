2d36031d6d2be6da5b26961afa8166602d1b6770
andy110414 <aeeoprsd110414@gmail.com>
2019-10-31 19:27:31 +0800

    測試

--- Propram/Program.cs
+++ Propram/Program.cs
@@ -7,6 +7,7 @@ namespace Propram
         static void Main(string[] args)
         {
             Calclators calc = new Calclators();
+
             Console.WriteLine($"3+5={calc.Add(3, 5)}");
             Console.WriteLine($"3-5={calc.Sub(3, 5)}");
             Console.WriteLine($"3*5={calc.Mul(3,5)}");
